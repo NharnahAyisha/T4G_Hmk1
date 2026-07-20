a7e22d9aca9d54e94b6dc532e566c2721256b89a
NharnahAyisha <nharnahaishakaylla@gmail.com>
2026-07-20 13:09:23 +0000

    add javascrit to my page

---
+++ src/style.js
@@ -0,0 +1,6 @@
+let username = prompt("Enter your name:"); //takes input from the user
+let age = prompt("Enter your age:"); //takes input from the user
+let email = prompt("Enter your email:"); //takes input from the user
+
+//alert message to display the user's name, age, and email address
+alert(`Hello ${username}, you are ${age} years old and we will keep in touch via ${email}`);
