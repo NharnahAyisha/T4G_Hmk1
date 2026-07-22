05457e0e8dc30b08ac6e5b41b54351510ed7db00
NharnahAyisha <nharnahaishakaylla@gmail.com>
2026-07-22 10:27:25 +0000

    added javasrcipt styling

--- src/style.js
+++ src/style.js
@@ -4,3 +4,33 @@ let email = prompt("Enter your email:"); //takes input from the user
 
 //alert message to display the user's name, age, and email address
 alert(`Hello ${username}, you are ${age} years old and we will keep in touch via ${email}`);
+
+let myName = "Ayishetu Dedei Armah";
+console.log(myName); 
+
+let myAge = 26;
+console.log(myAge);
+
+let addition = 10 + 5;
+console.log(addition);
+
+let subtraction = 10 - 5;
+console.log(subtraction);
+
+let division = 10 / 5;
+console.log(division);
+
+let multiplication = 10 * 5;
+console.log(multiplication);
+
+let remainder = 15 % 4;
+console.log(remainder);
+
+let isStudent = true;
+console.log(isStudent);
+
+let num = 50;
+console.log(typeof num);
+
+let text = "Frontend Development";
+console.log(typeof text);
