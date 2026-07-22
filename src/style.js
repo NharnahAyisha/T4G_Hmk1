424af4d973621ffd5d445e61254e5ed5a5925743
NharnahAyisha <nharnahaishakaylla@gmail.com>
2026-07-22 10:45:24 +0000

    made changes to the js

--- src/style.js
+++ src/style.js
@@ -5,34 +5,44 @@ let email = prompt("Enter your email:"); //takes input from the user
 //alert message to display the user's name, age, and email address
 alert(`Hello ${username}, you are ${age} years old and we will keep in touch via ${email}`);
 
+//store name and log it to the console
 let myName = "Ayishetu Dedei Armah";
 console.log("Name:", myName);
 
+//store age and log it to the console
 let myAge = 26;
 console.log("Age:", myAge);
 
+//store addition and log it to the console
 let addition = 10 + 5;
 console.log("Addition:", addition);
 
+//store subtraction and log it to the console
 let subtraction = 10 - 5;
 console.log("Subtraction:", subtraction);
 
+//store division and log it to the console
 let division = 10 / 5;
 console.log("Division:", division);
 
+//store multiplication and log it to the console
 let multiplication = 10 * 5;
 console.log("Multiplication:", multiplication);
 
+//store remainder and log it to the console
 let remainder = 15 % 4;
 console.log("Remainder:", remainder);
 
+//store myPage and log it to the console
 let myPage = true;
 console.log("My Page:", myPage);
 
+//store vitaValue and log it to the console
 let vitaValue = 50;
 console.log("Vita Value:", vitaValue);
 console.log("Type of Vita Value:", typeof vitaValue);
 
+//store healthValue and log it to the console
 let healthValue = "VitaBridge";
 console.log("Health Value:", healthValue);
 console.log("Type of Health Value:", typeof healthValue);
